b0eaf42b02fc8cf3d2e22c139e33d33a7b9c3527
unknown <star-q@outlook.com>
2022-12-16 03:52:12 -0400

    csv import dialog implemented

---
+++ src/components/ads-import-dialog.tsx
@@ -0,0 +1,105 @@
+import { useState, forwardRef, ReactElement, Ref } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Dialog,
+  List,
+  Divider,
+  Typography,
+  TransitionProps,
+  Slide,
+} from '@mui/material';
+
+import MUIDataTable from 'mui-datatables';
+
+import { CSVUploader } from './csv-uploader';
+
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: ReactElement;
+  },
+  ref: Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+interface ImportDialogProps {
+  open: boolean;
+  onSubmit: (columns: string[], data: string[][]) => void;
+  onCancel: () => void;
+}
+
+export default function AdsImportDialog({
+  open,
+  onSubmit,
+  onCancel,
+}: ImportDialogProps) {
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
+
+  const handleCSVUpload = (results: any) => {
+    setColumns(results[0]);
+    setData(results.slice(1));
+  };
+
+  const handleSubmit = () => {
+    onSubmit(columns, data);
+    setColumns([]);
+    setData([]);
+  };
+
+  const handleCancel = () => {
+    setColumns([]);
+    setData([]);
+    onCancel();
+  };
+
+  return (
+    <div>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleCancel}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: 'relative' }} color="secondary">
+          <Toolbar>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Import Ads
+            </Typography>
+            <Button
+              autoFocus
+              color="primary"
+              onClick={handleSubmit}
+              variant="contained"
+              sx={{ ml: 2 }}
+              disabled={data.length === 0}
+            >
+              Import
+            </Button>
+            <Button
+              autoFocus
+              color="error"
+              onClick={handleCancel}
+              variant="contained"
+              sx={{ ml: 2 }}
+            >
+              Cancel
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <List sx={{ mx: 2 }}>
+          <CSVUploader onUpload={handleCSVUpload} />
+          <Divider />
+          <MUIDataTable
+            title={'Employee List'}
+            data={data}
+            columns={columns}
+            options={{ selectableRowsHideCheckboxes: true }}
+          />
+        </List>
+      </Dialog>
+    </div>
+  );
+}
---
+++ src/components/csv-uploader.tsx
@@ -0,0 +1,161 @@
+import React, { useState, CSSProperties } from 'react';
+import { Box, Button } from '@mui/material';
+import {
+  useCSVReader,
+  formatFileSize,
+} from 'react-papaparse';
+
+const GREY = '#CCC';
+const GREY_LIGHT = 'rgba(255, 255, 255, 0.4)';
+const GREY_DIM = '#686868';
+
+const styles = {
+  zone: {
+    alignItems: 'center',
+    border: `2px dashed ${GREY}`,
+    borderRadius: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    justifyContent: 'center',
+    padding: 20,
+    minHeight: '164px'
+  } as CSSProperties,
+  file: {
+    background: 'linear-gradient(to bottom, #EEE, #DDD)',
+    borderRadius: 20,
+    display: 'flex',
+    height: 120,
+    width: 120,
+    position: 'relative',
+    zIndex: 10,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  } as CSSProperties,
+  info: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    paddingLeft: 10,
+    paddingRight: 10,
+  } as CSSProperties,
+  size: {
+    backgroundColor: GREY_LIGHT,
+    borderRadius: 3,
+    marginBottom: '0.5em',
+    justifyContent: 'center',
+    display: 'flex',
+  } as CSSProperties,
+  name: {
+    backgroundColor: GREY_LIGHT,
+    borderRadius: 3,
+    fontSize: 12,
+    marginBottom: '0.5em',
+  } as CSSProperties,
+  progressBar: {
+    bottom: 14,
+    position: 'absolute',
+    width: '100%',
+    paddingLeft: 10,
+    paddingRight: 10,
+  } as CSSProperties,
+  zoneHover: {
+    borderColor: GREY_DIM,
+  } as CSSProperties,
+  default: {
+    borderColor: GREY,
+  } as CSSProperties,
+  remove: {
+    height: 23,
+    position: 'absolute',
+    right: 6,
+    top: 6,
+    width: 23,
+  } as CSSProperties,
+};
+
+interface CSVUploaderProps {
+  onUpload: (data: any) => void;
+}
+
+export const CSVUploader = ({ onUpload }: CSVUploaderProps) => {
+  const { CSVReader } = useCSVReader();
+  const [zoneHover, setZoneHover] = useState(false);
+  const [removed, setRemoved] = useState(true);
+
+  const handleCSVUpload = (results: any) => {
+    onUpload(results.data);
+    setRemoved(false);
+    setZoneHover(false);
+  };
+
+  const handleFileRemove = () => {
+    onUpload([]);
+    setRemoved(true);
+  };
+
+  return (
+    <>
+      <Box>
+        <CSVReader
+          onUploadAccepted={handleCSVUpload}
+          onDragOver={(event: DragEvent) => {
+            event.preventDefault();
+            setZoneHover(true);
+          }}
+          onDragLeave={(event: DragEvent) => {
+            event.preventDefault();
+            setZoneHover(false);
+          }}
+        >
+          {({
+            getRootProps,
+            acceptedFile,
+            ProgressBar,
+          }: any) => (
+            <>
+              <div
+                {...getRootProps()}
+                style={Object.assign(
+                  {},
+                  styles.zone,
+                  zoneHover && styles.zoneHover
+                )}
+              >
+                {!removed && acceptedFile ? (
+                  <>
+                    <div style={styles.file}>
+                      <div style={styles.info}>
+                        <span style={styles.size}>
+                          {formatFileSize(acceptedFile.size)}
+                        </span>
+                        <span style={styles.name}>{acceptedFile.name}</span>
+                      </div>
+                      <div style={styles.progressBar}>
+                        <ProgressBar />
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  'Drop CSV file here or click to upload'
+                )}
+              </div>
+            </>
+          )}
+        </CSVReader>
+      </Box>
+      <Box
+        sx={{ my: 3, width: '100%', display: 'flex', justifyContent: 'flex-end' }}
+      >
+        <Button
+          onClick={handleFileRemove}
+          disabled={removed}
+          variant="outlined"
+          color="error"
+        >
+          Remove
+        </Button>
+      </Box>
+    </>
+  );
+};
